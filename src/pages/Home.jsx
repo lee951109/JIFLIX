e100cd670c63e170a94ae231b6ada24a59e0e015
lee951109 <sk985326@naver.com>
2022-07-22 12:33:05 +0900

    creat Banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Banner from "../components/Banner";
 import { movieAction } from "../redux/actions/movieAction";
 
 const Home = () => {
@@ -12,7 +13,14 @@ const Home = () => {
     dispatch(movieAction.getMovies());
   }, []);
 
-  return <div>Home!</div>;
+  return (
+    <div>
+      {
+        /* 조건부 랜더링 => popularMovies.results가 있으면 Banner를 보여준다*/
+        popularMovies.results && <Banner movie={popularMovies.results[0]} />
+      }
+    </div>
+  );
 };
 
 export default Home;
